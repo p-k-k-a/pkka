4c8f0297124424abbaec181b972c3d71c8f38e43
DEFALT314 <159141489+DEFALT314@users.noreply.github.com>
2026-05-08 12:02:58 +0200

    feat: update pre commit (#64)
    * feat: update pre commit
    
    * chore: rm git leaks

---
+++ backend/src/main/java/pl/edu/agh/backend/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package pl.edu.agh.backend.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("PKKA Backend API")
+                                .version("v1")
+                                .description(
+                                        "REST API for the Alumni Club platform of the AGH Faculty"
+                                                + " of Computer Science."))
+                .servers(
+                        List.of(
+                                new Server()
+                                        .url("http://localhost:8080")
+                                        .description("Local dev")));
+    }
+}
