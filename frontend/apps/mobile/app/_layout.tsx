9e52e6088073c280f326cdb8f4e98b57176c73d5
Paweł Gadomski <xpfk.098@gmail.com>
2026-05-17 14:48:50 +0200

    chore: remove expo template project

--- frontend/apps/mobile/app/_layout.tsx
+++ frontend/apps/mobile/app/_layout.tsx
@@ -1,25 +1,17 @@
-import { useColorScheme } from "@/hooks/use-color-scheme";
+import "@/global.css";
 import { NAV_THEME } from "@/lib/theme";
 import { ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import "../global.css";
-
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme: "light" | "dark" = "light";
 
   return (
-    <ThemeProvider value={NAV_THEME[colorScheme as "light" | "dark"]}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
-      </Stack>
+    <ThemeProvider value={NAV_THEME[colorScheme]}>
+      <Stack></Stack>
       <StatusBar style="auto" />
       <PortalHost />
     </ThemeProvider>
