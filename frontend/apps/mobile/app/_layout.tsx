0c7600c70eb4d81699e50e9b10c74c2af21cc171
Paweł Gadomski <xpfk.098@gmail.com>
2026-05-17 14:30:50 +0200

    chore: set up react-native-reusables

--- frontend/apps/mobile/app/_layout.tsx
+++ frontend/apps/mobile/app/_layout.tsx
@@ -1,9 +1,11 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { NAV_THEME } from "@/lib/theme";
+import { ThemeProvider } from "@react-navigation/native";
+import { PortalHost } from "@rn-primitives/portal";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-
-import { useColorScheme } from "@/hooks/use-color-scheme";
+import "../global.css";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -13,12 +15,13 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={NAV_THEME[colorScheme as "light" | "dark"]}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: "modal", title: "Modal" }} />
       </Stack>
       <StatusBar style="auto" />
+      <PortalHost />
     </ThemeProvider>
   );
 }
