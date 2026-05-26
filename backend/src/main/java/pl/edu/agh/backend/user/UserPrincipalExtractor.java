0d2a14fe2b24a49fb09c099b6e0d91796e9a7865
Radek <rad.szep@gmail.com>
2026-05-26 23:24:38 +0200

    add UserPrincipalExtractor

---
+++ backend/src/main/java/pl/edu/agh/backend/user/UserPrincipalExtractor.java
@@ -0,0 +1,34 @@
+package pl.edu.agh.backend.user;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserPrincipalExtractor {
+
+    public Optional<UserPrincipalInfo> extract(Authentication authentication) {
+        return switch (authentication) {
+            case OAuth2AuthenticationToken t when t.getPrincipal() instanceof OidcUser u ->
+                    Optional.of(new UserPrincipalInfo(
+                            u.getSubject(), u.getGivenName(), u.getFamilyName()));
+            case JwtAuthenticationToken t -> {
+                var jwt = t.getToken();
+                yield Optional.of(new UserPrincipalInfo(
+                        jwt.getSubject(),
+                        jwt.getClaimAsString("given_name"),
+                        jwt.getClaimAsString("family_name")));
+            }
+            default -> Optional.empty();
+        };
+    }
+
+    public record UserPrincipalInfo(
+            String keycloakId,
+            String firstName,
+            String lastName) {}
+}
